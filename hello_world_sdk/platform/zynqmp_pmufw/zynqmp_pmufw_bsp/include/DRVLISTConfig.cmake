47cad89addaa3d877177440055780c2111f08346
Soroush Javed Sulehri <saroushjaved@gmail.com>
2025-07-11 22:14:14 +0500

    Auto-commit on 11/07/2025 at 22:14:14.59

--- hello_world_sdk/platform/zynqmp_pmufw/zynqmp_pmufw_bsp/include/DRVLISTConfig.cmake
+++
@@ -1,1 +0,0 @@
-set(DRIVER_LIST avbuf;axipmon;canps;clockps;common;csudma;ddrcpsu;dpdma;dppsu;emacps;gpiops;iicps;ipipsu;pciepsu;qspipsu;resetps;rtcpsu;sdps;sysmonpsu;ttcps;uartps;usbpsu;video_common;wdtps;zdma)
